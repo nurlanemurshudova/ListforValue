77bd1e9f17495ff6149b33636500141727ad1dc6
nurlanemurshudova <nurlanamursudova@gmail.com>
2024-04-06 09:19:24 +0400

    Add project files.

---
+++ ListforValue/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace ListforValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value value = new Value();
+            ValueEdit edit = new ValueEdit();
+            bool loopCheck = true;
+            while (loopCheck)
+            {
+                Console.WriteLine("1-add integer value");
+                Console.WriteLine("2-delete integer value");
+                Console.WriteLine("3-show integer values");
+                Console.WriteLine("4-updated the selected integer value");
+                Console.WriteLine("5-quit");
+
+                bool check = int.TryParse(Console.ReadLine(), out int number);
+                if (check)
+                {
+                    switch (number)
+                    {
+                        case 1:
+                            Console.WriteLine("Ededin id-ni daxil et");
+                            bool checkId = int.TryParse(Console.ReadLine(), out int setId);
+                            Console.WriteLine("Eded daxil et");
+                            bool checkNum = int.TryParse(Console.ReadLine(), out int setNum);
+
+                            if (checkNum && checkId)
+                            {
+                                Value value1 = new Value()
+                                {
+                                    MyValue = setNum,
+                                    Id = setId
+                                };
+                                edit.AddValue(value1);
+                            }
+                            else
+                                Console.WriteLine("Integer eded daxil et");
+                            break;
+                        case 2:
+                            Console.WriteLine("Silmek istediyin reqemin id-ni daxil et");
+                            checkNum = int.TryParse(Console.ReadLine(), out int deleteNum);
+                            if (checkNum)
+                                edit.DeleteValue(deleteNum);
+                            else
+                                Console.WriteLine("Integer eded daxil et");
+                            break;
+                        case 3:
+                            var getAllValue = edit.ShowValues();
+                            foreach (var item in getAllValue)
+                            {
+                                Console.WriteLine($"{item.Id}. {item.MyValue}");
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Deyismek istediyin ededin id-ni daxil et");
+                            bool tryChangeId = int.TryParse(Console.ReadLine(), out int tryId);
+                            Console.WriteLine("Yeni ededi daxil et");
+                            bool tryChangeValue = int.TryParse(Console.ReadLine(), out int tryValue);
+                            if (tryChangeValue)
+                            {
+                                Value value2 = new Value()
+                                {
+                                    Id = tryId,
+                                    MyValue = tryValue
+                                };
+
+                                edit.UpdateValue(value2);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Dogru reqem daxil edin");
+                            }
+                            break;
+                        case 5:
+                            loopCheck = false;
+                            break;
+                        default:
+                            Console.WriteLine("1 2 3 4 5 sec");
+                            break;
+                    }
+
+                }
+                else
+                    Console.WriteLine("Reqem daxil et");
+
+            }
+
+        }
+    }
+    class Value
+    {
+        public int Id { get; set; }
+        public int MyValue { get; set; }
+
+    }
+    class ValueEdit
+    {
+        List<Value> values = new List<Value>();
+        public void AddValue(Value value)
+        {
+            values.Add(value);
+        }
+        public void DeleteValue(int i)
+        {
+            var deleteValue = values.Find(x => x.Id == i);
+            values.Remove(deleteValue);
+        }
+        public List<Value> ShowValues()
+        {
+            return values;
+        }
+        public List<Value> UpdateValue(Value value)
+        {
+            foreach (var item in values)
+            {
+                if (item.Id == value.Id)
+                {
+                    item.MyValue = value.MyValue;
+                    break;
+                }
+            }
+            return values;
+        }
+    }
+}
